1aa3902b79e757fb4259d1dd2625c69e844d0e68
Yannpyc <yann.fournier@ynov.com>
2022-10-05 17:51:06 +0200

    main func

---
+++ findword.go
@@ -0,0 +1,33 @@
+package hangman
+
+import (
+	"math/rand"
+)
+
+// compresser le dossier en .zip (pas en .rar(sinon modification de ))
+
+func Findword(s []byte) string {
+	mot := ""
+	tab := bytetoword(s)
+	long := len(tab)
+	nbr := rand.Intn(long)
+	mot = tab[nbr]
+	return mot
+}
+
+func bytetoword(tab []byte) []string {
+	tabl := []string{}
+	str := ""
+	if len(tab) == 0 {
+		return tabl
+	}
+	for i := 0; i < len(tab); i++ {
+		if tab[i] != 10 {
+			str += string(rune(tab[i]))
+		} else if tab[i] == 10 {
+			tabl = append(tabl, str)
+			str = ""
+		}
+	}
+	return tabl
+}
---
+++ fonctionprinc/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"hangman"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+	for true {
+		str := ""
+		fmt.Println("1 : Lancer une nouvelle partie")
+		fmt.Println("2 : Continuer la partie sauvegarder")
+		fmt.Println("3 : En savoir à propos du jeu")
+		fmt.Println("4 : Quitter le jeu")
+		fmt.Printf("Ecris le numéro de ce que tu veux faire : ")
+		fmt.Scan(&str)
+		switch str {
+		case "1":
+			fmt.Println("Une nouvelle partie a été lancée")
+			fmt.Printf("\n")
+
+			Mot := ""
+			// Trouver le mot à deviner
+			if len(os.Args[1:]) == 0 {
+				fmt.Printf("File name missing\n")
+			} else if len(os.Args[1:]) > 1 {
+				fmt.Printf("Too many arguments\n")
+			} else {
+				contents, err := os.ReadFile(os.Args[1])
+				if err != nil {
+					fmt.Println("File reading error", err)
+					return
+				}
+				Mot = hangman.Findword(contents)
+				//fmt.Println(mot)
+
+				// Initialisation des varibles
+				var cpt int
+				var lettremanque int
+				Ascci := [][]string{}
+				for i := 0; i < len(Mot); i++ {
+					Ascci = append(Ascci, []string{})
+				}
+
+				// afficher aléatoirement n lettres
+				n := len(Mot)/2 - 1
+				tab := []int{}
+				boolean := false
+				for boolean == false {
+					for i := 0; i < n; i++ {
+						rand.Seed(time.Now().UnixNano())
+						radomInt2 := rand.Intn(len(Mot))
+						tab = append(tab, radomInt2)
+					}
+				}
+				//Ascci = Reprandletter(Ascci, tab)
+
+				for (cpt != 10) || (lettremanque != 0) {
+					fmt.Printf("Good Luck, you have %v attempts.", 10-cpt)
+					fmt.Printf("\n")
+					fmt.Print("Ecrire des lettres en minuscules s'il vous plaît")
+					var lettre string
+					fmt.Scan(&lettre)
+					for i := 0; i < len(Mot); i++ {
+						if lettre == string(Mot[i]) {
+							//Ascci[i] = hangman.Lettertoascci(lettre)
+							lettremanque--
+						} else {
+							cpt++
+							//hangman.Posehang(cpt)
+						}
+					}
+				}
+				if cpt == 10 {
+					fmt.Println("Dommage, lance une nouvelle partie pour réessayer")
+				} else if lettremanque == 0 {
+					fmt.Printf("Bravo, tu as trouvé le bon mot qui était %v", Mot)
+				}
+
+			}
+
+		case "2":
+			fmt.Println("Continuer la partie sauvegardée")
+			fmt.Printf("\n")
+		case "3":
+			fmt.Println("readme")
+			fmt.Printf("\n")
+		case "4":
+			return
+		}
+	}
+}
